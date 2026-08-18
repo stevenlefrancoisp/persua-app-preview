daef61170bd69913982855201aecf14d8a3a4d16
Steven Lefrancois <stevenlefrancois@macbook-air-de-steven.home>
2026-08-18 18:57:12 +0200

    Déconnexion accessible partout (Profil + vue coach) + logout robuste. SW v40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'persua-preview-v39';
+const CACHE = 'persua-preview-v40';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (e) => {
